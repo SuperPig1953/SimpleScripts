1ce312402a3c5f977ea02155dc7d985969d71112
cPig <100575272+SuperPig1953@users.noreply.github.com>
2023-05-29 13:57:52 +0700

    Simple ActionFormData

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,27 @@
 import * as mc from '@minecraft/server'
+import * as ui from '@minecraft/server-ui'
 // UnHandle Scripting Watchdog FIX
-mc.system.events.beforeWatchdogTerminate.subscribe(event =>{
+mc.system.events.beforeWatchdogTerminate.subscribe(event => {
     event.cancel = true
 })
-//Code
+let fm = new ui.ActionFormData()
+.title("TITLE")
+.body("BODY OF FORM")
+.button("free diamond")//0
+.button("2")//1
+.button("button 3")//2
 
-
-mc.world.events.blockBreak.subscribe(event =>{
-
-    event.player.applyKnockback(1,1,1,1)
-    event.player.sendMessage("block broken")
-})
-
-mc.world.events.beforeChat.subscribe(event =>{
-    event.cancel = true
-    event.sender.sendMessage(`${event.sender.nameTag} sent : ${event.message}`)
+mc.world.events.itemUse.subscribe(event => {
+    fm.show(event.source).then(res => {
+        switch (res.selection){
+            case 0:
+                event.source.runCommandAsync('give @s diamond')
+                break
+            case 1:
+                event.source.applyKnockback(0,0,1,1)
+                break
+            case 2:
+                break
+        }
+    })
 })
